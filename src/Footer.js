6fde0563cbe22a1a4a2afaad96c8d48b95e21e60
Hannah Bernard <hannahbernarddesign@gMAIL.COM>
2020-05-29 15:33:29 -0400

    handle enter and done

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ContinuousSlider from "./ContinuousSlider";
 
 const Footer = props => {
+
+    const handleKeyPress = (event) => {
+        if(event.which === 13){
+            props.submit(event)
+        }
+    }
+
     if (props.isAvailable) {
         return (
             <footer>
                 <input type="number" pattern="[0-9]*" inputMode="numeric" min={1} max={55} defaultValue={props.page}
-                       onFocus={props.onfocus} onBlur={props.onblur} onChange={props.submit}/>
+                       onFocus={props.onfocus} onBlur={props.onblur} onChange={props.submit} onKeyPress={handleKeyPress}/>
                 <div className="slide-container">
                     <ContinuousSlider change={props.change} commit={props.commit} value={props.page}
                                       history={props.history}></ContinuousSlider>
