72d0cc6c0f75839a37bb62d352049e945f28a7a4
Hannah Bernard <hannahbernarddesign@gMAIL.COM>
2020-06-03 09:10:11 -0400

    visd updates

--- src/BookPopUp.js
+++ src/BookPopUp.js
@@ -6,7 +6,7 @@ import bookmarkSvg from './images/bookmark.svg';
 
 const BookPopUp = props => {
   const arrowLeft = (props.page * 5.8);
-  const rowLeft = -(props.page * 12);
+  const rowLeft = -(props.page * 14);
   const pageList = [];
   for(var i = 1; i < 56; i++){
      let image;
--- src/ContinuousSlider.js
+++ src/ContinuousSlider.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Slider from '@material-ui/core/Slider';
 
 const ContinuousSlider = props => {
+
+  let past = props.value > 1 ? props.history : null;
+
   return (
     <div>
-          <Slider value={props.value} min={1} max={55} onChange={props.change} onChangeCommitted={props.commit} marks={[{value:props.history}]} />
+          <Slider value={props.value} min={1} max={55} onChange={props.change} onChangeCommitted={props.commit} marks={[{value:past}]} />
     </div>
   );
 }
